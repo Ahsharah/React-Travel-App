d326e15aa6d59924d42389186f5d26762fdac9f6
Alexandria <walkerciara24@gmail.com>
2024-12-10 00:12:53 -0500

    Added DestinationList component to display and toggle destinations

--- src/components/DestinationList/DestinationList.js
+++ src/components/DestinationList/DestinationList.js
@@ -1,22 +1,46 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import "./DestinationList.css";
+import { addDestination } from "../../store/destinationsSlice";
 
 const DestinationList = () => {
-  const destinations = useSelector((state) => state.destinations);
+  const destinations = useSelector((state) => state.destinations.list);
+  const dispatch = useDispatch();
+
+  const toggleVisited = (id) => {
+    const updatedDestinations = destinations.map((destination) =>
+      destination.id === id
+        ? { ...destination, visited: !destination.visited }
+        : destination
+    );
+    dispatch(addDestination(updatedDestinations));
+  };
 
   return (
     <div className="destination-list">
-      <h2>Destinations</h2>
-      {destinations.length ? (
+      <h2>Your Destinations</h2>
+      {destinations.length === 0 ? (
+        <p className="no-destinations">No destinations added yet!</p>
+      ) : (
         <ul>
-          {destinations.map((dest) => (
-            <li key={dest.id}>
-              <strong>{dest.name}</strong> - {dest.description}
+          {destinations.map((destination) => (
+            <li key={destination.id} className="destination-item">
+              <h3>{destination.name}</h3>
+              <p>{destination.description}</p>
+              <p>
+                <strong>Best Time to Visit:</strong> {destination.bestTime}
+              </p>
+              <button
+                className={`toggle-visited ${
+                  destination.visited ? "visited" : "not-visited"
+                }`}
+                onClick={() => toggleVisited(destination.id)}
+              >
+                {destination.visited ? "Visited" : "Not Visited"}
+              </button>
             </li>
           ))}
         </ul>
-      ) : (
-        <p>No destinations added yet.</p>
       )}
     </div>
   );
